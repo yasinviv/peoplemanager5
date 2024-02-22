78372940ffa5cff0999eb1be0cd6d7261699bd10
yasinviv <159425350+yasinviv@users.noreply.github.com>
2024-02-22 09:43:34 +0100

    Initial commit

---
+++ PeopleManager.Ui.Mvc/Controllers/PeopleController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PeopleManager.Ui.Mvc.Controllers
+{
+    public class PeopleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
